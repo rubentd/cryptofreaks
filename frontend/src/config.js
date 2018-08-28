3aadb16011300e6fc8e89708a15529ac4ecc1db2
Ruben Torres <rubentdlh@gmail.com>
2018-08-27 22:29:43 -0300

    Sell, stop sale, gift monsters

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const CONTRACT_NAME = 'CryptoFreaks';
-export const CONTRACT_ADDRESS = '0xaf7442774156caf2bfd8503a09355ebfa8a3757b';
+export const CONTRACT_ADDRESS = '0xfa986462d1e4bb2d20a3c5ee7dfdaf7d0518571f';
--- frontend/src/containers/MonsterPage.js
+++ frontend/src/containers/MonsterPage.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { bool, string, object } from 'prop-types';
+import Modal from 'react-modal';
 import MonsterImage from 'components/MonsterImage';
 import generateName from 'util/generateName';
 import '@fortawesome/fontawesome-free/js/all';
 import { CONTRACT_NAME } from 'config';
+
+Modal.setAppElement('#root');
+
+const modalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    width: 350,
+    height: 200,
+    marginLeft: -175,
+    marginTop: -100, 
+  },
+};
 
 class MonsterPage extends React.Component {
   static propTypes = {
@@ -21,17 +35,90 @@ class MonsterPage extends React.Component {
       error: false,
       txInProgress: false,
       tx: '',
+      showSellModal: false,
+      showGiftModal: false,
     };
+  }
+
+  componentWillMount() {
+    this.contract = this.context.drizzle.contracts[CONTRACT_NAME];
   }
 
   buyMonster = () => {
     const { id, price } = this.props;
-    const contract = this.context.drizzle.contracts[CONTRACT_NAME];
     const currentUser = window.web3.eth.defaultAccount; 
 
-    contract.methods.buyMonster(id).send({ 
+    this.contract.methods.buyMonster(id).send({ 
       from: currentUser,
       value: price,
+    }).then((result) => {
+      this.setState({
+        txInProgress: true,
+        txHash: result.transactionHash,
+      });
+    }).catch(() => {
+      this.setState({
+        error: true,
+        txInProgress: false,
+      });
+    });
+  }
+
+  sellMonster = () => {
+    const { id } = this.props;
+    const currentUser = window.web3.eth.defaultAccount;
+    const price = window.web3.toWei(parseFloat(this.priceInput.value));
+
+    this.contract.methods.putOnSale(id, price).send({ 
+      from: currentUser,
+    }).then((result) => {
+      this.setState({
+        txInProgress: true,
+        txHash: result.transactionHash,
+        showSellModal: false,
+      });
+    }).catch(() => {
+      this.setState({
+        error: true,
+        txInProgress: false,
+        showSellModal: false,
+      });
+    });
+  }
+
+  giftMonster = () => {
+    const { id } = this.props;
+    const currentUser = window.web3.eth.defaultAccount;
+    const recipient = this.addressInput.value;
+
+    if (!window.web3.isAddress(recipient)) {
+      alert('Please specify a valid address');
+      return;
+    }
+
+    this.contract.methods.giftMonster(id, recipient).send({ 
+      from: currentUser,
+    }).then((result) => {
+      this.setState({
+        txInProgress: true,
+        txHash: result.transactionHash,
+        showGiftModal: false,
+      });
+    }).catch(() => {
+      this.setState({
+        error: true,
+        txInProgress: false,
+        showGiftModal: false,
+      });
+    });
+  }
+
+  stopSellingMonster = () => {
+    const { id } = this.props;
+    const currentUser = window.web3.eth.defaultAccount; 
+
+    this.contract.methods.removeFromSale(id).send({ 
+      from: currentUser,
     }).then((result) => {
       this.setState({
         txInProgress: true,
@@ -54,14 +141,19 @@ class MonsterPage extends React.Component {
       price,
       owner,
     } = this.props;
-    const { txInProgress, txHash } = this.state;
+    const {
+      txInProgress,
+      txHash,
+      showSellModal,
+      showGiftModal,
+    } = this.state;
 
     const isMine = owner.toLowerCase() === window.web3.eth.defaultAccount.toLowerCase();
 
     return (
       <div className="monster-page">
         {
-          forSale && (
+          forSale && !txInProgress && (
             <div className="monster-sale-info">
               <div>
                 For Sale &nbsp;
@@ -78,7 +170,11 @@ class MonsterPage extends React.Component {
               {
                 isMine && !forSale && (
                   <li>
-                    <button>
+                    <button onClick={() => {
+                      this.setState({
+                        showSellModal: true,
+                      });
+                    }}>
                       Sell &nbsp;<i className="fa fa-tag"></i>
                     </button>
                   </li>
@@ -87,7 +183,7 @@ class MonsterPage extends React.Component {
               {
                 isMine && forSale && (
                   <li>
-                    <button>
+                    <button onClick={this.stopSellingMonster}>
                       Stop Sale &nbsp;<i className="fa fa-tag"></i>
                     </button>
                   </li>
@@ -96,7 +192,11 @@ class MonsterPage extends React.Component {
               {
                 isMine && (
                   <li>
-                    <button>
+                    <button onClick={() => {
+                      this.setState({
+                        showGiftModal: true,
+                      });
+                    }}>
                       Gift &nbsp;<i className="fa fa-gift"></i>
                     </button>
                   </li>
@@ -114,6 +214,28 @@ class MonsterPage extends React.Component {
             </ul>
           )
         }
+
+        <Modal isOpen={showSellModal} style={modalStyle}>
+          <label>Price in ETH:</label>
+          <input ref={(e) => { this.priceInput = e;}} type="number" defaultValue={0.01} min={0.00001} />
+          <p className="modal-options">
+            <button className="button-clear" onClick={() => { this.setState({ showSellModal: false });}}>
+              Close
+            </button>
+            <button onClick={this.sellMonster}>Sell</button>
+          </p>
+        </Modal>
+
+        <Modal isOpen={showGiftModal} style={modalStyle}>
+          <label>Recipient address:</label>
+          <input ref={(e) => { this.addressInput = e;}} type="text" />
+          <p className="modal-options">
+            <button className="button-clear" onClick={() => { this.setState({ showGiftModal: false });}}>
+              Close
+            </button>
+            <button onClick={this.giftMonster}>Gift Monster</button>
+          </p>
+        </Modal>
 
         <MonsterImage genes={genes}/>
         <div className="monster-info">
